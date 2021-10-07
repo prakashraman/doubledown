2e505e56f3d3a0b1c2450730d01d52ba10cfffee
Prakash Raman <prakash.raman.ka@gmail.com>
2021-10-07 15:06:48 +0200

    config variables added

---
+++ lib/config.ts
@@ -0,0 +1,5 @@
+const CONFIG = {
+  LOGTAIL_SOURCE_TOKEN: process.env.LOGTAIL_SOURCE_TOKEN,
+};
+
+export default CONFIG;
--- lib/init.ts
+++ lib/init.ts
@@ -1,8 +1,15 @@
-import { createLogger, transports } from "winston";
+import { createLogger, transports, format } from "winston";
+import { Logtail } from "@logtail/node";
+import { LogtailTransport } from "@logtail/winston";
+
+import CONFIG from "./config";
 
 /**
  * The main logger
  */
 export const logger = createLogger({
-  transports: [new transports.Console()],
+  transports: [
+    new transports.Console(),
+    new LogtailTransport(new Logtail(CONFIG.LOGTAIL_SOURCE_TOKEN)),
+  ],
 });
