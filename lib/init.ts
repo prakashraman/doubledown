95c0ac55e4027bb0f4cfa7d03ca9c2caf2ce5f6d
Prakash Raman <prakash.raman.ka@gmail.com>
2021-10-08 10:41:13 +0200

    removed logtail

--- lib/init.ts
+++ lib/init.ts
@@ -1,15 +1,8 @@
-import { createLogger, transports, format } from "winston";
-import { Logtail } from "@logtail/node";
-import { LogtailTransport } from "@logtail/winston";
-
-import CONFIG from "./config";
+import { createLogger, transports } from "winston";
 
 /**
  * The main logger
  */
 export const logger = createLogger({
-  transports: [
-    new transports.Console(),
-    new LogtailTransport(new Logtail(CONFIG.LOGTAIL_SOURCE_TOKEN)),
-  ],
+  transports: [new transports.Console()],
 });
