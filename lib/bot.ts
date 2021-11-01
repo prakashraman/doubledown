fdccf72c69d775d3f002b6acdd380b600a3a0829
Prakash Raman <prakash.raman.ka@gmail.com>
2021-11-01 19:09:15 +0100

    updatd HOT

--- lib/bot.ts
+++ lib/bot.ts
@@ -21,7 +21,7 @@ interface Model {
 
 const model: Model = {
   symbol: "HOTBUSD",
-  price: 0.015227,
+  price: 0.01271111,
 };
 
 /**
--- lib/notify.ts
+++ lib/notify.ts
@@ -3,6 +3,11 @@ import CONFIG from "./config";
 
 const web = new WebClient(CONFIG.SLACK_TOKEN);
 
+/**
+ * Sends a message to slack
+ *
+ * @param {string} m
+ */
 const sendMessage = async (m: string) => {
   await web.chat.postMessage({
     text: m,
