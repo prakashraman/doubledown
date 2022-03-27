7fe4d66e9679bbfdaa0386ac2b258c27e393e87e
Prakash Raman <prakash.raman.ka@gmail.com>
2022-03-27 17:04:12 +0530

    purchase at 4% decrease

--- app.ts
+++ app.ts
@@ -12,39 +12,10 @@ import * as db from "./lib/db";
 
 logger.info("Starting worker ...", { time: new Date() });
 
-/**
- * Runs the process at the frequency mentioned in the cron
- *
- * Go through the "run" method to figure things out.
- */
 const bot = new CronJob("*/10 * * * * *", run);
-
-/**
- * Runs the collective bot every 10 seconds (or see cron spec below)
- *
- * Go through the "collective.run" method to figure things out
- */
 const botCollective = new CronJob("*/10 * * * * *", collective.run);
-
-/**
- * Runs the mint bot every 20 seconds (or see cron spec below)
- *
- * Go through the "mint.run" method to figure things out
- */
 const botMint = new CronJob("*/20 * * * * *", mint.run);
-
-/**
- * Update the account balances
- *
- * Runs every minutes and updates the database with all the balances
- */
 const balances = new CronJob("* * * * *", updateBalances);
-
-/**
- * Runs the mint bot every 20 seconds (or see cron spec below)
- *
- * Go through the "splitshort.run" method to figure things out
- */
 const botSplitshort = new CronJob("*/20 * * * * *", splitshort.run);
 
 /**
